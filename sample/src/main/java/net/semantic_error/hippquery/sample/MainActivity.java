dc49028862110e20ba2c268568dbfc9b6603e057
Tomáš Valenta <semanticer@gmail.com>
2015-12-19 16:59:19 +0100

    Finished version 0.0.1

---
+++ sample/src/main/java/net/semantic_error/hippquery/sample/MainActivity.java
@@ -0,0 +1,30 @@
+package net.semantic_error.hippquery.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import net.semantic_error.hippquery.Query;
+import net.semantic_error.hippquery.sample.R;
+
+import static net.semantic_error.hippquery.util.Functions.sum;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String q = Query.select("Name", sum("points"))
+                .from("Users AS u")
+                .join("Result AS r").on("r.user = u.id")
+                .where("u.name LIKE 'Something'").and("u.age > 18")
+                .groupBy("u.id")
+                .having(sum("points") + ">100")
+                .orderBy("u.age")
+                .toString();
+
+        Log.d("MainActivity", q);
+    }
+}
