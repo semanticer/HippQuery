c0d8ef6e11512de486e787de1f6f64df3d9f2586
Tomáš Valenta <semanticer@gmail.com>
2015-12-29 14:52:12 +0100

    Added documentation

--- library/src/main/java/net/semantic_error/hippquery/scope/BaseScope.java
+++ library/src/main/java/net/semantic_error/hippquery/scope/BaseScope.java
@@ -4,5 +4,10 @@ package net.semantic_error.hippquery.scope;
  * Created by semanticer on 19. 12. 2015.
  */
 public interface BaseScope {
+    /**
+     * Provides a method to insert raw sql query part to your query
+     * @param queryPart part or full sql query
+     * @return new query with raw part on the end
+     */
     AllScopes raw(String queryPart);
 }
--- library/src/main/java/net/semantic_error/hippquery/scope/JoinScope.java
+++ library/src/main/java/net/semantic_error/hippquery/scope/JoinScope.java
@@ -5,5 +5,13 @@ package net.semantic_error.hippquery.scope;
  */
 public interface JoinScope extends BaseScope {
     FromScope on(String condition);
+
+    /**
+     * Convenience method for creating most used form of ON statement with equals condition between two columns.
+     * on(col1,col2) is equivalent to on(col1=col2) see {@link this#on(String)}
+     * @param firstColumn of the condition
+     * @param secondColumn of the condition
+     * @return new Query with new ON statement at the end
+     */
     FromScope on(String firstColumn, String secondColumn);
 }
