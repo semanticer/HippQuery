6ba4f8c773e5a05de74ecd8147d64959113a5a04
Tomáš Valenta <semanticer@gmail.com>
2016-01-01 19:35:23 +0100

    Improvements to sample code - sample query

--- sample/src/main/java/net/semantic_error/hippquery/sample/LibraryRepository.java
+++ sample/src/main/java/net/semantic_error/hippquery/sample/LibraryRepository.java
@@ -7,6 +7,9 @@ import net.semantic_error.hippquery.sample.AuthorContract.AuthorkEntry;
 import net.semantic_error.hippquery.sample.BookContract.BookEntry;
 import net.semantic_error.hippquery.Query;
 
+import static net.semantic_error.hippquery.util.Functions.max;
+import static net.semantic_error.hippquery.util.Functions.sum;
+
 /**
  * Created by semanticer on 30. 12. 2015.
  */
@@ -19,9 +22,16 @@ public class LibraryRepository {
         db = openHelper.getWritableDatabase();
     }
 
-    Cursor getBestAuthors() {
-        String query = Query.select(AuthorkEntry.COLUMN_LAST_NAME)
-                .from(AuthorkEntry.COLUMN_FIRST_NAME)
+    Cursor getMostProductioveAuthors(int topCount) {
+        String query = Query
+                .select(AuthorkEntry.COLUMN_FIRST_NAME, AuthorkEntry.COLUMN_LAST_NAME, sum(BookEntry.COLUMN_PAGE_COUNT))
+                .from(AuthorkEntry.TABLE_NAME, "a")
+                .join(BookEntry.TABLE_NAME, "b")
+                .on("a."+ AuthorkEntry._ID, "b." + BookEntry.COLUMN_AUTHOR)
+                .groupBy("a."+AuthorkEntry._ID)
+                .orderBy(sum(BookEntry.COLUMN_PAGE_COUNT))
+                .limit(0)
+                .offset(topCount)
                 .toString();
 
         return db.rawQuery(query, null);
